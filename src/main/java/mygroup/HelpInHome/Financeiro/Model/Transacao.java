ca158e72f434d5e23f2a71e591b52246d5ad5c5c
Lindenjefferson Borges <lindenjefferson@outlook.com>
2022-04-17 22:58:20 -0300

    Iniciando o projeto

--- src/main/java/mygroup/HelpInHome/Financeiro/Model/Transacao.java
+++ src/main/java/mygroup/HelpInHome/Financeiro/Model/Transacao.java
@@ -3,35 +3,23 @@ package mygroup.HelpInHome.Financeiro.Model;
 import java.io.Serializable;
 import java.sql.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class Transacao implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
+@Table(name = "transacao")
+public class Transacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
+    private Long id;
     private Date data;
     private Float valor;
     private String descricao;
-
     @ManyToOne
-    @Column(nullable = false)
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
-
-    @ManyToOne
-    @Column(nullable = true)
-    private Planejamento planejamento;
 
 }
